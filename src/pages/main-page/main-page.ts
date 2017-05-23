e4bbc46be0790efd8323dd193ec3ff2bb8a22e0f
PatrickFis <fischerpl@mail.lipscomb.edu>
2017-05-23 09:05:21 -0500

    Added a temporary logo to the login page. Added stock photos of trainers to the main page. Also changed the slides to be generated from content present in main-page.ts. Centered the buy workouts button on the buy-workouts page.

--- src/pages/main-page/main-page.ts
+++ src/pages/main-page/main-page.ts
@@ -8,4 +8,21 @@ export class MainPage {
   constructor() {
 
   }
+  slides = [
+    {
+      title: "Trainer 1",
+      description: "Description here",
+      image: "assets/img/trainer1.jpg",
+    },
+    {
+      title: "Trainer 2",
+      description: "Description here",
+      image: "assets/img/trainer2.jpg",
+    },
+    {
+      title: "Trainer 3",
+      description: "Description here",
+      image: "assets/img/trainer3.jpg",
+    }
+  ];
 }
